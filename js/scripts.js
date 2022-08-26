435d3520194cbd9a98f684dc4c16b1c30536d462
NeaMitika <59248866+NeaMitika@users.noreply.github.com>
2022-08-26 16:07:11 +0200

    Aggiunto Animate.css e fatto il Menu responsive

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,24 @@ new Glider(document.querySelector('.contenitore-cards'), {
     exactWidth: true,
     });
 })
+
+const navigazione = document.querySelector('.nagivazione-mobile');
+
+function ToggleNavigazione() {
+
+    if (navigazione.classList.contains('animate__slideOutLeft')) {
+
+        navigazione.classList.add('animate__slideInLeft');
+        navigazione.classList.remove('animate__slideOutLeft');
+
+      } else if (navigazione.classList.contains('animate__slideInLeft')) {
+
+        navigazione.classList.add('animate__slideOutLeft');
+
+      } else {
+
+        navigazione.classList.add('animate__slideOutLeft');
+        navigazione.style.display = 'block';
+
+      }
+}
